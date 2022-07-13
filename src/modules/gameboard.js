3f768dff2e5d59527be6da129fcb8879175efd79
Angie Nguyen <angie.ta.nguyen@gmail.com>
2022-07-13 16:57:02 -0700

    placeShip not longer public and done at initialization

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -1,9 +1,12 @@
 import { Ship } from "./ship";
 
-const Gameboard = () => {
+//shipLocations in format [[[x1,y1],[x2,y2],...], [x1, y1], ...]]]
+const Gameboard = shipLocations => {
     let ships = [];
     let grid;
-    
+    //Array contents in string format for legalMoves.
+    let legalMoves = [];
+
     const Location = () => {
         let containedShip = null;
         let isHit = false;
@@ -14,22 +17,28 @@ const Gameboard = () => {
         grid = new Array(10);
         for(let r = 0; r < grid.length; r++) {
             grid[r] = new Array(10);
-            for(let c = 0; c < grid[r].length; c++) grid[r][c] = Location();
+            for(let c = 0; c < grid[r].length; c++) {
+                grid[r][c] = Location();
+                legalMoves.push("[" + r + "," + c + "]");
+            }
         }
     })();
 
-    const placeShip = locations => {
-        let ship = Ship(locations);
-        ships.push(ship);
-        for(let i = 0; i < locations.length; i++) {
-            let gridLocation = grid[locations[i][0]][locations[i][1]];
-            gridLocation.containedShip = ship;
-        }
-    }
+    const placeShips = (() => {
+        shipLocations.forEach(individualShip => {
+            let ship = Ship(individualShip);
+            ships.push(ship);
+            individualShip.forEach(position => {
+                let gridLocation = grid[position[0]][position[1]];
+                gridLocation.containedShip = ship;
+            });
+        });
+    })();
 
     const receiveAttack = (x, y) => {
-        if(grid[x][y].containedShip !== null) grid[x][y].containedShip.hit(x, y);
+        if(grid[x][y].containedShip != null) grid[x][y].containedShip.hit(x, y);
         grid[x][y].isHit = true;
+        gameboard.legalMoves.splice(gameboard.legalMoves.indexOf("[" + x + "," + y + "]"), 1);
     }
 
     const checkIfAllSunk = () => {
@@ -39,16 +48,17 @@ const Gameboard = () => {
     }
 
     const checkIfLocationHitAndMissed = (x, y) => {
-        if(grid[x][y].isHit && grid[x,y].containedShip === null) return true;
+        if(grid[x][y].isHit && grid[x][y].containedShip == null) return true;
         return false;
     }
 
     const checkIfLocationHitWithShip = (x, y) => {
-        if(grid[x][y].isHit && grid[x,y].containedShip !== null) return true;
+        if(grid[x][y].isHit && grid[x][y].containedShip != null) return true;
         return false;
     }
 
-    return {placeShip, receiveAttack, checkIfAllSunk, checkIfLocationHitAndMissed, checkIfLocationHitWithShip};
+    const gameboard = {legalMoves, receiveAttack, checkIfAllSunk, checkIfLocationHitAndMissed, checkIfLocationHitWithShip};
+    return gameboard;
 }
 
 export {Gameboard};
