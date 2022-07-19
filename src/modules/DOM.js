529dba692659ceb6c157830901dcee9f4513aed1
Angie Nguyen <angie.ta.nguyen@gmail.com>
2022-07-19 11:09:24 -0700

    Can move ships after placing (but not around original spot)

--- src/modules/DOM.js
+++ src/modules/DOM.js
@@ -204,20 +204,197 @@ const initialSetup = (() => {
             });
         }
 
+        let lastHoveredOverDivs;
+        function addDropOffEvent(d, i) {
+            let validPosition;
+            let hoveredOverDivs = [];
+
+            function createDivForHover(isValid) {
+                let hoverDiv = document.createElement("div");
+                hoverDiv.classList.add("hover-effect");
+                if(isValid) hoverDiv.style.backgroundColor = "white";
+                else hoverDiv.style.backgroundColor = "red";
+                return hoverDiv;
+            }
+
+            d.ondragenter = e => {
+                console.log("dragged over: " + d);
+                e.preventDefault();
+                if(lastHoveredOverDivs != undefined) removeDragStyles(lastHoveredOverDivs);
+                hoveredOverDivs = [];
+
+                let grabbedItem = document.getElementsByClassName("grabbed")[0];
+                let size = [...grabbedItem.getElementsByTagName("div")].length;
+                let isHorizontal = grabbedItem.getElementsByTagName("div")[0].classList.contains("revealed-ship-left");
+
+                if(isHorizontal && (!checkIfCoordinateAllowsShip(i) || findHorizontalCoordinatesIfFit(size, i) == null)) {
+                    console.log("No horizontal spots");
+                    let offset = 0;
+                    while((i + offset) < 100 && offset < size && Math.floor(i / 10) === Math.floor((i + offset) / 10)) {
+                        let div = document.querySelectorAll("#setup-board > div")[i + offset];
+                        div.append(createDivForHover(false));
+                        hoveredOverDivs.push(div);
+                        offset++;
+                    }
+                    validPosition = false;
+                }
+                else if(!isHorizontal && (!checkIfCoordinateAllowsShip(i) || findVerticalCoordinatesIfFit(size, i) == null)) {
+                    console.log("No vertical spots");
+                    let offset = 0;
+                    while((i + offset) < 100 && offset < size * 10 && i % 10 === (i + offset) % 10){
+                        let div = document.querySelectorAll("#setup-board > div")[i + offset];
+                        div.append(createDivForHover(false));
+                        hoveredOverDivs.push(div);
+                        offset += 10;
+                    } 
+                    validPosition = false;
+                }
+                else {
+                    let max = size * 10;
+                    let change = 10;
+                    if(isHorizontal) {
+                        change = 1;
+                        max = size;
+                    }
+
+                    for(let offset = 0; offset < max; offset += change) {
+                        let div = document.querySelectorAll("#setup-board > div")[i + offset];
+                        div.append(createDivForHover(true));
+                        hoveredOverDivs.push(div);
+                    }
+                    validPosition = true;
+                }
+                lastHoveredOverDivs = hoveredOverDivs;
+                console.log(hoveredOverDivs);
+            }
+
+            d.ondragover = e => {
+                e.preventDefault();
+                e.dataTransfer.dropEffect = "move";
+            }
+
+            d.ondrop = e => {
+                e.preventDefault();
+                let originalSections = [...document.getElementsByClassName("original-section")];
+                
+                if(validPosition) {
+                    let ship = e.dataTransfer.getData("ship");
+                    let shipDiv = document.createElement("div");
+                    shipDiv.innerHTML = ship;
+                    let shipSections = shipDiv.getElementsByTagName("div");
+                    hoveredOverDivs.forEach(h => {
+                        shipSections[0].setAttribute("draggable", true);
+                        h.replaceWith(shipSections[0]);
+                    });
+
+                    console.log("original: ");
+                    console.log(originalSections);
+                    if(originalSections.length === 0) document.getElementsByClassName("grabbed")[0].remove();
+                    else {
+                        originalSections.forEach(o => {
+                            let index = Array.prototype.indexOf.call(document.querySelectorAll("#setup-board > div"), o);
+                            let replacement = document.createElement("div");
+                            addDropOffEvent(replacement, index);
+                            o.replaceWith(replacement);
+                        });
+                    }
+                }
+                else if (originalSections.length !== 0) {
+                    originalSections.forEach(o => o.classList.remove("original-section"));
+                }
+                
+                let coordinates = [];
+                hoveredOverDivs.forEach((h,offset) => coordinates.push(i + offset));
+                playerCoordinates.push(coordinates);
+                hoveredOverDivs = [];
+            }
+        }
+
+        [...dragDropOffs].forEach((d,i) => {
+            addDropOffEvent(d,i);
+        });
+
+        function sectionDragEvents() {
+            let dragSections = [...document.getElementsByClassName("draggable-section")];
+
+            dragSections.forEach(s => {
+                let specialClass;
+                for(let i = 0; i < s.classList.length; i++) {
+                    let item = s.classList.item(i);
+                    let regex = /draggable-ship-*/;
+                    if(regex.test(item)) specialClass = item; 
+                }
+                let specialClassShips = [...document.getElementsByClassName(specialClass)];
+        
+                s.onmouseenter = e => {
+                    specialClassShips.forEach(sc => {
+                        if(sc.classList.contains("revealed-ship-middle")) sc.style.backgroundColor = "rgb(55, 55, 55)";
+                        else sc.getElementsByTagName("i")[0].style.color = "rgb(55, 55, 55)";
+                    });
+                }
+
+                s.onmouseleave = e => {
+                    specialClassShips.forEach(sc => {
+                        if(sc.classList.contains("revealed-ship-middle")) sc.style.backgroundColor = "var(--ship-color)";
+                        else sc.getElementsByTagName("i")[0].style.color = "var(--ship-color)";
+                    });
+                }
+
+                s.ondragstart = e => {
+                    let div = document.createElement("div");
+                    div.classList.add("drag-image");
+                    div.classList.add("drag-ship");
+                    div.classList.add("grabbed");
+                    div.style.display = "flex";
+
+                    let isHorizontal = specialClassShips[0].classList.contains("revealed-ship-left");
+                    if(!isHorizontal) div.style.flexDirection = "column";
+
+                    document.getElementsByTagName("body")[0].append(div);
+                    specialClassShips.forEach(c => {
+                        let sectionClone = c.cloneNode(true);
+                        div.append(sectionClone);
+                        c.classList.add("original-section");
+                    });
+                    e.dataTransfer.setDragImage(div, 0, 0);
+
+                    e.dataTransfer.setData("ship", div.innerHTML);
+                    e.dataTransfer.setData("size", specialClassShips.length);
+                    e.dataTransfer.setData("isHorizontal", isHorizontal);
+                }
+
+                s.ondragend = e => {
+                    removeDragStyles([...document.querySelectorAll("#setup-board > div")]);
+                    document.getElementsByClassName("drag-image")[0].remove();
+                    sectionDragEvents();
+                }
+            });
+        }
+
         [...draggables].forEach((d,i) => {
             let shipSection = d.getElementsByTagName("div");
-            [...shipSection].forEach(s => s.classList.add("draggable-ship-" + i));
-
+            [...shipSection].forEach(s => {
+                s.classList.add("draggable-ship-" + i)
+                s.classList.add("draggable-section");
+            });
             d.ondragstart = e => {
                 d.classList.add("grabbed");
                 e.dataTransfer.setData("ship", d.innerHTML);
                 e.dataTransfer.setData("size", [...shipSection].length);
-                e.dataTransfer.setData("isHorizontal", [...shipSection][0].classList.item(0) === "revealed-ship-left");
+                e.dataTransfer.setData("isHorizontal", [...shipSection][0].classList.contains("revealed-ship-left"));
+
+                let clone = d.cloneNode(true);
+                clone.style.background = "none";
+                clone.classList.add("drag-image");
+                document.getElementsByTagName("body")[0].append(clone);
+                e.dataTransfer.setDragImage(clone, 0, 0);
             }
 
             d.ondragend = e => {
                 d.classList.remove("grabbed");
-                removeDragStyles([...dragDropOffs]);
+                removeDragStyles([...document.querySelectorAll("#setup-board > div")]);
+                document.getElementsByClassName("drag-image")[0].remove();
+                sectionDragEvents();
             }
 
             let shipIcons = d.getElementsByTagName("i");
@@ -240,95 +417,6 @@ const initialSetup = (() => {
                 }
             }
         });
-
-        let lastHoveredOverDivs;
-        [...dragDropOffs].forEach((d,i) => {
-            let validPosition;
-            let hoveredOverDivs = [];
-
-            function createDivForHover(isValid) {
-                let hoverDiv = document.createElement("div");
-                hoverDiv.classList.add("hover-effect");
-                if(isValid) hoverDiv.style.backgroundColor = "white";
-                else hoverDiv.style.backgroundColor = "red";
-                return hoverDiv;
-            }
-
-            d.ondragenter = e => {
-                e.preventDefault();
-                if(lastHoveredOverDivs != undefined) removeDragStyles(lastHoveredOverDivs);
-                hoveredOverDivs = [];
-
-                let grabbedItem = document.getElementsByClassName("grabbed")[0];
-                let size = [...grabbedItem.getElementsByTagName("div")].length;
-                let isHorizontal = grabbedItem.getElementsByTagName("div")[0].classList.item(0) === "revealed-ship-left";
-
-                if(isHorizontal && (!checkIfCoordinateAllowsShip(i) || findHorizontalCoordinatesIfFit(size, i) == null)) {
-                    console.log("No horizontal spots");
-                    let offset = 0;
-                    while((i + offset) < 100 && offset < size && Math.floor(i / 10) === Math.floor((i + offset) / 10)) {
-                        let div = dragDropOffs[i + offset];
-                        div.append(createDivForHover(false));
-                        hoveredOverDivs.push(div);
-                        offset++;
-                    }
-                    validPosition = false;
-                }
-                else if(!isHorizontal && (!checkIfCoordinateAllowsShip(i) || findVerticalCoordinatesIfFit(size, i) == null)) {
-                    console.log("No vertical spots");
-                    let offset = 0;
-                    while((i + offset) < 100 && offset < size * 10 && i % 10 === (i + offset) % 10){
-                        let div = dragDropOffs[i + offset];
-                        div.append(createDivForHover(false));
-                        hoveredOverDivs.push(div);
-                        offset += 10;
-                    } 
-                    validPosition = false;
-                }
-                else {
-                    let max = size * 10;
-                    let change = 10;
-                    if(isHorizontal) {
-                        change = 1;
-                        max = size;
-                    }
-
-                    for(let offset = 0; offset < max; offset += change) {
-                        let div = dragDropOffs[i + offset];
-                        div.append(createDivForHover(true));
-                        hoveredOverDivs.push(div);
-                    }
-                    validPosition = true;
-                }
-                lastHoveredOverDivs = hoveredOverDivs;
-            }
-
-            d.ondragover = e => {
-                e.preventDefault();
-                e.dataTransfer.dropEffect = "move";
-            }
-
-            d.ondrop = e => {
-                e.preventDefault();
-                console.log("dropped");
-                
-                if(validPosition) {
-                    let ship = e.dataTransfer.getData("ship");
-                    let shipDiv = document.createElement("div");
-                    shipDiv.innerHTML = ship;
-                    let shipSections = shipDiv.getElementsByTagName("div");
-                    hoveredOverDivs.forEach(h => {
-                        h.replaceWith(shipSections[0]);
-                    });
-                    document.getElementsByClassName("grabbed")[0].remove();
-                }
-                
-                let coordinates = [];
-                hoveredOverDivs.forEach((h,offset) => coordinates.push(i + offset));
-                playerCoordinates.push(coordinates);
-                hoveredOverDivs = [];
-            }
-        });
     })();
 
     function convert1Dto2DCoordinates(coordinates1D) {
@@ -345,7 +433,6 @@ const initialSetup = (() => {
     //updates player coordinates
     document.getElementById("randomizer-btn").onclick = () => {
         clearBoard(setupBoard);
-        console.log(setupBoard.innerHTML);
         playerCoordinates = randomizeShipPlacement().coordinates;
     }
 
@@ -377,3 +464,36 @@ const initialSetup = (() => {
         }
     }
 })();
+
+const setUpRound = () => {    
+    const setPlayerTurnView = player => {
+        let gridSquare = document.getElementById("bot-board").getElementsByTagName("div");
+        [...gridSquare].forEach(square, index => {
+            let x = Math.floor(index / 10);
+            let y = index % 10;
+            if(player.checkIfAbleToAttackLocation(x,y)) {
+                square.onclick = () => {
+                    player.attack(x,y);
+                };
+                square.onmouseover = () => {square.style.backgroundColor = "rgb(7, 155, 219)"};
+            }
+        });
+    }
+    
+    const applyStylesForAttackLocation = (ship, div) => {
+        let icon = document.createElement("i");
+        icon.classList.add("fa-solid");
+        if(ship == null) {
+            icon.classList.add("fa-circle");
+            div.append(icon);
+            div.classList.add("missed-shot");
+        }
+        else if(ship == null) {
+            icon.classList.add("fa-explosion");
+            div.append(icon);
+            div.classList.add("hit-ship");
+        }
+    
+    }
+    return {setPlayerTurnView};
+}
