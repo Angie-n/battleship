893040a8648ceea45c54d1e142b8e3936dc298a7
Angie Nguyen <angie.ta.nguyen@gmail.com>
2022-07-21 15:26:00 -0700

    Added test for getShipSunkByLastAttack()

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,5 +1,5 @@
 import { Gameboard } from "../modules/gameboard";
-import { indexOf2DArray } from "../modules/helper";
+import { indexOf2DArray, sort2DCoordinates } from "../modules/helper";
 
 let board;
 
@@ -32,4 +32,15 @@ test("Place one ship at spanning [5,5], [6,5], [7,5] and hit [5,5]", () => {
     expect(board.checkIfAllSunk()).toBe(false);
     expect(indexOf2DArray(board.legalMoves, [5,5])).toBe(-1);
     expect(indexOf2DArray(board.legalMoves, [7,5])).not.toBe(-1);
+});
+
+test("Place a ship spanning [0, 0], [1, 0], [2,0], [3, 0] and another at [9, 2], [9, 3]. Check getShipSunkByLastAttack() before and after sinking first ship.", () => {
+    board = Gameboard([[[0, 0], [1, 0], [2,0], [3, 0]], [[9, 2], [9, 3]]]);
+    expect(board.getShipSunkByLastAttack()).toBe(null);
+    board.receiveAttack(0,0);
+    board.receiveAttack(1,0);
+    expect(board.getShipSunkByLastAttack()).toBe(null);
+    board.receiveAttack(2,0);
+    board.receiveAttack(3,0);
+    expect(sort2DCoordinates(board.getShipSunkByLastAttack())).toEqual([[0, 0], [1, 0], [2, 0], [3, 0]]);
 });
